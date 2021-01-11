6947f476ff05cf8af8779d6102a5f9322b5bed33
Diego Delgado <ddphillyfan@gmail.com>
2021-01-11 14:17:07 -0500

    Added filtering

--- force-app/main/default/lwc/dataTable/dataTable.js
+++ force-app/main/default/lwc/dataTable/dataTable.js
@@ -2,23 +2,14 @@ import { LightningElement, wire, track } from 'lwc';
 import getCaseList from '@salesforce/apex/CasesController.getCaseList';
 import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { refreshApex } from '@salesforce/apex'
-
+//import { refreshApex } from '@salesforce/apex';
 
 
 const columns = [
     {label: 'Id', fieldName: 'Id', editable: false},
     {label: 'Case Number', fieldName: 'CaseNumber', editable: false},
-    {
-        label: 'Priority',
-        fieldName: 'Priority',
-        editable: true,
-    },
-    {
-        label: 'Status',
-        fieldName: 'Status',
-        editable: true,
-    },
+    {label: 'Priority',fieldName: 'Priority',editable: true},
+    {label: 'Status',fieldName: 'Status',editable: true},
     {label: 'Type', fieldName: 'Type', editable: true},
     {label: 'Subject', fieldName: 'Subject', editable: true},
     {label: 'Contact Email', fieldName: 'ContactEmail', type: 'email', editable: true},
@@ -27,57 +18,13 @@ const columns = [
 export default class DataTable extends LightningElement {
 
     // Initialize variables
-    records;
+    @track records;
+    @track initialRecords;
     selectedValue = 'All';
-    sortedColumn;
-    sortedDirection;
-    columns = columns;
     draftValues = [];
-    ALL_CASES = [];
-    cases = [];
-    contactResult = null;
+    columns = columns;
 
-
-    // Wire the getCaseList() method from Apex Controller
-    // @wire(getCaseList)
-    // contact;
     
-    // // Use getCaseList to assign data to cases and ALL_CASES to be used when filtering
-    // @track data;
-    // @wire(getCaseList)
-    // wiredCases({ error, data }) {
-    //     if (data) {
-    //         this.ALL_CASES = data;
-    //         this.cases = data;
-    //         console.log(this.cases)
-    //         this.error = undefined;
-    //     } else if (error) {
-    //         this.error = error;
-    //         this.contacts = undefined;
-    //     }
-
-    // }
-
-        
-    get options() {
-        return [
-            { label: 'All', value: 'All' },
-            { label: 'High', value: 'High' },
-            { label: 'Medium', value: 'Medium' },
-            { label: 'Low', value: 'Low' }
-        ];
-    }
-
-    handleChange(event) {
-        this.selectedValue = event.target.value;
-        if (this.selectedValue === 'All') {
-            this.records = this.initialRecords;
-        }
-        else{
-            this.filter();
-        }
-    }
-
     @wire(getCaseList)
     wiredCases({error, data}) {
 
@@ -92,33 +39,29 @@ export default class DataTable extends LightningElement {
         }
     }
 
-    sortRecs(event) {
-        let colName = event ? event.target.name : undefined;
-        console.log("Column Name is: " + colName);
-
-        if(this.sortedColumn === colName) {
-            this.sortedDirection = (
-                this.sortedDirection === 'asc' ? 'desc' : 'asc'
-            );
-        } else {
-            this.sortedDirection = 'asc';
-        }
-
-        let isReverse = this.sortedDirection === 'asc' ? 1:-1;
-
-        if(colName) {
-            this.sortedColumn = colName;
-        } else {
-            colName = this.sortedColumn;
-        }
-
-        this.records = JSON.parse(JSON.stringify(this.records)).sort((a, b) =>{
-            a = a[ colName ] ? a[ colName ].toLowerCase() : 'z';
-            b = b[ colName ] ? b[ colName ].toLowerCase() : 'z';
-            return a > b ? 1 * isReverse : -1 * isReverse;
-        });
+        
+    get options() {
+        return [
+            { label: 'All', value: 'All' },
+            { label: 'High', value: 'High' },
+            { label: 'Medium', value: 'Medium' },
+            { label: 'Low', value: 'Low' }
+        ];
     }
 
+    
+    // Handles change of combobox values
+    handleChange(event) {
+        this.selectedValue = event.target.value;
+        if (this.selectedValue === 'All') {
+            this.records = this.initialRecords;
+        }
+        else{
+            this.filter();
+        }
+    }
+
+    // Filters records based on the selectedValue in the combobox
     filter() {
         if(this.selectedValue) {
             this.records = this.initialRecords;
@@ -157,7 +100,8 @@ export default class DataTable extends LightningElement {
             // Clear out draft values
             this.draftValues = [];
             // Use refreshApex to update list view w/o refreshing the page
-            return refreshApex(this.contact)
+            window.location.reload();
+            // return refreshApex(this.contact)
         }).catch(error => {
             // On error show error message
             this.showToastMsg('Error Updating Case', error.body.message, error)
@@ -175,36 +119,4 @@ export default class DataTable extends LightningElement {
             })
         )
     }
-
-
-    // handleHeaderAction(event) {
-    //     // gives the selection header action name
-    //     const actionName = event.detail.action.name;
-    //     console.log("Action Name: " + actionName)
-
-    //     // gives selected column definition
-    //     const colDef = event.detail.columnDefinition;
-    //     console.log("Column Defenition: " + JSON.stringify(colDef));
-
-    //     // assigning colmuns to new variable
-    //     let cols = this.columns;
-    //     console.log("Columns: " + JSON.stringify(cols));
-
-    //     if (actionName !== undefined && actionName !== 'all') {
-    //         // filtering cases on selected actionname
-    //         console.log(this.cases)
-    //         this.cases = this.ALL_CASES.filter(_case => _case[colDef.label] === actionName);
-    //         console.log(`All ${colDef.label} cases: ` + JSON.stringify(this.cases))
-    //     } else if (actionName === 'all') {
-    //         // returning all cases
-    //         this.cases = this.ALL_CASES;
-    //         console.log(this.cases);
-    //     }
-
-    //     /* Following line is responsible for finding which header action selected and return corresponding actions then we will mark selcted as checked/true and remaining will be marked as unchecked/marked */
-
-    //     cols.find(col => col.label === colDef.label).actions.forEach(action => action.checked = action.name === actionName);
-    //     this.columns = [...cols];
-    //     console.log("End");
-    // }
 }
